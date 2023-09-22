d68af81405fe15c408bd665302c93330452cf46f
Nikita Obergan <Obergannikita@gmail.com>
2023-09-22 18:45:45 -0400

    End of the day displaying book title and author and description in bookdetails page but when you refresh everything is gone

--- src/components/BookDetailsPage/BookDetailsPage.jsx
+++ src/components/BookDetailsPage/BookDetailsPage.jsx
@@ -1,7 +1,26 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 const BookDetailsPage = () => {
   const { bookId } = useParams();
+  const [bookItem, setBookItem] = useState([]);
+
+  const fetchBook = async () => {
+    try {
+      let response = await axios.get(
+        `https://www.googleapis.com/books/v1/volumes/${bookId}`
+      );
+      setBookItem(response.data);
+      console.log(response.data);
+    } catch (error) {
+      console.log("Eror in fetchBook by id in Book DetailsPage ", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchBook();
+  }, []);
 
   return (
     <div className="d-flex justify-content-evenly">
@@ -12,16 +31,14 @@ const BookDetailsPage = () => {
             <button>Remove Favorite</button>
           </div>
         </div>
-        <div>Name</div>
-        <div>Author</div>
-        <div>Discription</div>
+        <div>Title: {bookItem.volumeInfo.title}</div>
+        <div>Author: {bookItem.volumeInfo.authors}</div>
+        <div>Discription: {bookItem.volumeInfo.description}</div>
       </div>
 
       <div className="p-2 w-50%">
         <div>Average User Rating: 4.5</div>
-        <div>
-          <table>User Reviews</table>
-        </div>
+        <div>User Reviews Table</div>
         <div>Container to leave a review</div>
       </div>
     </div>
