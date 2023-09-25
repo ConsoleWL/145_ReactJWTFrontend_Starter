0e380517ac1b9825a392257f534e8878f5f3ed4d
Nikita Obergan <Obergannikita@gmail.com>
2023-09-25 14:31:47 -0400

    fix BookDetailsPage add null checl

--- src/components/BookDetailsPage/BookDetailsPage.jsx
+++ src/components/BookDetailsPage/BookDetailsPage.jsx
@@ -4,7 +4,10 @@ import { useParams } from "react-router-dom";
 
 const BookDetailsPage = () => {
   const { bookId } = useParams();
-  const [bookItem, setBookItem] = useState([]);
+  const [bookItem, setBookItem] = useState(null);
+  const [bookReviews, setBookReviews] = useState([]);
+
+  console.log(`book item ${bookItem}`);
 
   const fetchBook = async () => {
     try {
@@ -12,41 +15,56 @@ const BookDetailsPage = () => {
         `https://www.googleapis.com/books/v1/volumes/${bookId}`
       );
       setBookItem(response.data);
-      console.log(response.data);
+      console.log(`Book details ${response.data}`);
     } catch (error) {
       console.log("Eror in fetchBook by id in Book DetailsPage ", error);
     }
   };
 
+  // const fetchReviews = async () => {
+  //   try {
+  //     let response2 = await axios.get(
+  //       `https://localhost:5001/api/BookDetails/${bookId}`
+  //     );
+  //     setBookReviews(response2);
+  //     console.log(`Book details ${response2}`);
+  //   } catch (error) {
+  //     console.log("Error in fetch reviews by Book id in Book DetailsPage");
+  //   }
+  // };
+
   useEffect(() => {
     fetchBook();
   }, []);
 
-  return (
-    <div className="d-flex justify-content-evenly">
-      <div className="p-2 w-50%">
-        <div className="d-flex align-items-start">
-          <div>book image</div>
-          <div>
-            <button>Remove Favorite</button>
-          </div>
-        </div>
-        <div>Title: {bookItem.volumeInfo.title}</div>
-        <div>Author: {bookItem.volumeInfo.authors}</div>
-        <div>Discription: {bookItem.volumeInfo.description}</div>
-      </div>
+  // useEffect(() => {
+  //   fetchReviews();
+  // fetchReviews();
+  // }, []);
 
-      <div className="p-2 w-50%">
-        <div>Average User Rating: 4.5</div>
-        <div>User Reviews Table</div>
-        <div>Container to leave a review</div>
+  return (
+    bookItem && (
+      <div className="d-flex justify-content-evenly">
+        <div className="p-2 w-50%">
+          <div className="d-flex align-items-start">
+            <img src={bookItem.volumeInfo.imageLinks.smallThumbnail} />
+            <div>
+              <button>Remove Favorite</button>
+            </div>
+          </div>
+          <div>Title: {bookItem.volumeInfo.title}</div>
+          <div>Author: {bookItem.volumeInfo.authors}</div>
+          <div>Discription: {bookItem.volumeInfo.description}</div>
+        </div>
+
+        <div className="p-2 w-50%">
+          <div>Average User Rating: 4.5</div>
+          <div>User Reviews Table</div>
+          <div>Container to leave a review</div>
+        </div>
       </div>
-    </div>
+    )
   );
 };
 
 export default BookDetailsPage;
-
-{
-  /* https://www.googleapis.com/books/v1/volumes/{Book Id here} */
-}
